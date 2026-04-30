d96a236192853de6f72ac8986a5dc2861f3d3ab3
samubj <125109942+samubj@users.noreply.github.com>
2026-04-30 11:35:27 +0530

    Update Task-4.js

--- Task-4.js
+++ Task-4.js
@@ -9,6 +9,7 @@ let arr = [10, 20, 30, 40, 50];
 console.log("First:", arr[0]);
 console.log("Last:", arr[arr.length - 1]);
 console.log("Length:", arr.length);
+
 // Output:
 // First: 10
 // Last: 50
